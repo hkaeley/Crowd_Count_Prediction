b341a29032e40cf5baa497450867a72982306f82
hkaeley <harsimratkaeley@gmail.com>
2022-10-07 18:11:55 -0700

    Data loading & saving

--- data/data.py
+++ data/data.py
@@ -1,8 +1,77 @@
+import json
+from tqdm import tqdm
+import sklearn.model_selection
+import numpy as np
+import sys, os
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+import pickle as pkl
+import json
+import cv2
 
-
-
-class Dataset_Parser():
-    pass
-
+                    
 class Dataset():
-    pass
+
+    def __init__(self, args):
+        self.args = args
+        self.dataset_path = args.dataset_file_path
+        self.dataset_save_path = args.dataset_save_path
+        self.width = args.img_width
+        self.height = args.img_height
+        
+        
+    #store data into two lists
+    def extract_data(self):
+        self.images = []
+        self.labels = []
+
+        if not os.path.exists(self.dataset_path):
+            raise FileNotFoundError(self.dataset_path)
+
+        image_folder_names = [x for x in Path(self.dataset_path).iterdir() if x.is_dir() and "images" in x.stem]
+        json_folder = [x for x in Path(self.dataset_path).iterdir() if x.is_dir() and "jsons" in x.stem][0]
+        
+        data_count = 0
+
+        for image_folder in image_folder_names:
+            images = [Path(f) for f in image_folder.iterdir() if isfile(f) and "jpg" in f.suffix]
+            for image_ in tqdm(range(len(images))):
+                image = images[image_]
+                im = self._load_image(image)
+                self.images.append(im)
+                image_name = image.stem.split(".")[0]
+                self.labels.append(self.find_label(json_folder, image_name))
+                data_count += 1
+                if self.args.data_count != 0 and data_count == self.args.data_count: #0 will indicate extract all data
+                    break
+            if self.args.data_count != 0 and data_count == self.args.data_count:
+                break
+
+    #find label associated with given image
+    def find_label(self, label_folder, image_name):
+        label_file_path = (label_folder/(image_name + '.json')).resolve()
+        with open(label_file_path) as f:
+            data = json.load(f)
+            return int(data["human_num"])
+
+    #Represent each image as a tensor            
+    def _load_image(self, image_path):
+        im = cv2.imread(str(image_path), 1)
+        im = cv2.resize(im, (self.width, self.height)) #TODO: see what sizes papers using this dataset use
+        self.im_height, self.im_width, self.color_channels = im.shape
+        return im
+
+    def split_dataset(self):
+        self.train_data_x, self.test_data_x, self.train_data_y, self.test_data_y = sklearn.model_selection.train_test_split(self.images, self.labels, train_size = 0.7, random_state = 42) #split into train and test
+        self.images, self.labels = [], []
+
+    #load/save data from dataset_path into data, labels, meta
+    def save(self):
+        with open(self.dataset_save_path, 'wb') as f:
+            pkl.dump(self, f)
+
+    def load(self):
+        with open(self.dataset_save_path, 'rb') as f:
+            return pkl.load(f)
+        
--- train/train.py
+++ train/train.py
@@ -1,55 +1,70 @@
-from dataset import Dataset
 from argparse import ArgumentParser
 import torch
 from tqdm import tqdm
-from model import StockFormer, lstm, StockFormerEncoder
 import numpy as np
 import sys, os
 from pathlib import Path
 import wandb 
 
-sys.path.append("../")
+sys.path.append("../data")
+from data import Dataset
+sys.path.append("../model")
 from model import *
-from train import *
 
 class Trainer():
-    def __init__(self) -> None:
+    def __init__(self, args) -> None:
+        self.args = args
+        self.data = Dataset(args)
+
+    def build_dataset(self):
+        if not self.args.load_dataset:
+            self.data.extract_data()
+            self.data.split_dataset()
+            self.data.save()
+        else:
+            self.data = self.data.load()
+
+    def build_model(self):
+        #TODO
         pass
 
-    def build_dataset():
-        pass
-    def split_dataset():
-        pass
-    def build_model():
-        pass
-    def train():
+    def train(self):
+        #TODO
         pass
 
 if __name__ == "__main__":
         ap = ArgumentParser(description='The parameters for training.')
-        ap.add_argument('--dataset_file_path', type=str, default="C:/Users/harsi/research/stockformer/StockFormer/data.csv", help="The path defining location of indicator dataset.")
+        ap.add_argument('--load_dataset', type=bool, default = True)
+        ap.add_argument('--dataset_file_path', type=str, default="../../dataset", help="The path defining location of indicator dataset.")
+        ap.add_argument('--dataset_save_path', type=str, default="../../dataset.pkl", help="The path defining save/load location of processed dataset pkl.")
+        ap.add_argument('--img_width', type=int, default=60, help="Resized width")
+        ap.add_argument('--img_height', type=int, default=60, help="Resized height")
+        ap.add_argument('--data_count', type=int, default=10, help="Number of images to extract")
+
+
         ap.add_argument('--epochs', type=int, default = 50)
         ap.add_argument('--device', type=str, default = "cuda:0")
         ap.add_argument('--test_step', type=int, default = 5)
         ap.add_argument('--batch_size', type=int, default = 16)
         ap.add_argument('--optimizer', type=str, default = "Adam")
         ap.add_argument('--learning_rate', type=float, default = 0.0001)
-        ap.add_argument('--log_wandb', type=str, default = "True")
 
+        ap.add_argument('--log_wandb', type=str, default = "True")
         ap.add_argument('--wandb_project', type=str, default = "") #TODO
         ap.add_argument('--wandb_entity', type=str, default = "") #TODO
+
+        ap.add_argument('--model', type=str, default = "CrowdModel")
         ap.add_argument('--model_save_directory', type=str, default = "saved_models/")
-        ap.add_argument('--model_save_file', type=str, default = "model_best_val_loss_.vec.pt")
-        ap.add_argument('--model_load_path', type=str, default = "saved_models/model_best_val_loss_.vec.pt")
+        ap.add_argument('--model_save_file', type=str, default = "best_model.pt")
+        ap.add_argument('--model_load_path', type=str, default = "saved_models/best_model.pt")
         ap.add_argument('--load_model', type=bool, default = False)
-        ap.add_argument('--model', type=str, default = "stockformer")
+        
         #include more args here if we decide to do ensemble training?
 
 
         args = ap.parse_args()
         trainer = Trainer(args)
         trainer.build_dataset()
-        trainer.split_dataset()
         trainer.build_model()
         trainer.train()
 
