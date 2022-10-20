a6df324d348c37f53d275869e4b2e166e5317cd8
hkaeley <harsimratkaeley@gmail.com>
2022-10-19 22:47:37 -0700

    Initial Model & Trainer push

--- model/model.py
+++ model/model.py
@@ -1,3 +1,97 @@
+import torch.nn as nn
+import torch
 
-class CrowdModel():
-    pass
+class SimpleCrowdModel(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        self.layer1 = nn.Sequential(
+            # 1. define a COnv2d layer with parameters: input_channel, output_channel, kernel_size, stride, padding
+            # input_channel: the input
+            # ouput channel:  how many feature map we are generating
+            # kernel size: Convolutional filter size
+            # padding: zero padding      
+            # ***In order to determine the parameter values, you need to calculate them based on the given dataset***
+            nn.Conv2d(in_channels = in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding), #Your Code#
+            
+            # 2. define a BatchNorm2d layer for normalizing the output from the previous layer input, to let the layer nn learn independently, improving performance
+            nn.BatchNorm2d(num_features = out_channels), #Your Code#)
+
+            # 3. define a ReLU activation
+            #Your Code#
+            nn.ReLU(),
+
+            # 4. define a MaxPooling layer for dividing current feature map size by the maxpool kernel size
+            nn.MaxPool2d(kernel_size = 3, stride = 1) #stride = None by default #Your Code#)
+            )
+        self.layer2 = nn.Sequential(
+            # Layer2 should have the same network structure of layer1
+            # 1. define a COnv2d layer with parameters: input_channel, output_channel, kernel_size, stride, padding
+             nn.Conv2d(in_channels = out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding), #Your Code#
+            
+            # 2. define a BatchNorm2d layer for normalizing the output from the previous layer input, to let the layer nn learn independently, improving performance
+            nn.BatchNorm2d(num_features = out_channels), #Your Code#)
+
+            # 3. define a ReLU activation
+            #Your Code#
+            nn.ReLU(),
+
+            # 4. define a MaxPooling layer for dividing current feature map size by the maxpool kernel size
+            nn.MaxPool2d(kernel_size = 3, stride = 1) #stride = None by default #Your Code#)
+            )
+           
+
+        self.layer3 = nn.Sequential(
+            # Layer2 should have the same network structure of layer1
+            # 1. define a COnv2d layer with parameters: input_channel, output_channel, kernel_size, stride, padding
+            nn.Conv2d(in_channels = out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding), #Your Code#
+            
+            # 2. define a BatchNorm2d layer for normalizing the output from the previous layer input, to let the layer nn learn independently, improving performance
+            nn.BatchNorm2d(num_features = out_channels), #Your Code#)
+
+            # 3. define a ReLU activation
+            #Your Code#
+            nn.ReLU(),
+
+            # 4. define a MaxPooling layer for dividing current feature map size by the maxpool kernel size
+            nn.MaxPool2d(kernel_size = 3, stride = 1) #stride = None by default #Your Code#)
+            )
+           
+        
+        self.layer4 = nn.Sequential(
+            # Layer2 should have the same network structure of layer1
+            # 1. define a COnv2d layer with parameters: input_channel, output_channel, kernel_size, stride, padding
+             nn.Conv2d(in_channels = out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding), #Your Code#
+            
+            # 2. define a BatchNorm2d layer for normalizing the output from the previous layer input, to let the layer nn learn independently, improving performance
+            nn.BatchNorm2d(num_features = out_channels), #Your Code#)
+
+            # 3. define a ReLU activation
+            #Your Code#
+            nn.ReLU(),
+
+            # 4. define a MaxPooling layer for dividing current feature map size by the maxpool kernel size
+            nn.MaxPool2d(kernel_size = 3, stride = 1) #stride = None by default #Your Code#)
+            )
+        
+        
+        # define output layer which flattens the convolutional layer output
+        # this layer layer indicates how many pixel positions we have in our output feature map from the latest convolution layer
+
+        #each conv layer pretty much takes away 4 from height and width due to the conv2d and maxpool2d layers (see their formulas for more info)
+        # therefore last lin layer needs to take as input out_channels * (initial_height - num_layers * 4) * (initial_width - num_layers * 4) 
+        initial_height, initial_width = 28, 28
+        num_layers = 4
+        self.input_fts = out_channels * (initial_height - num_layers * 4) * (initial_width - num_layers * 4) 
+        self.fc = nn.Linear(in_features = self.input_fts, out_features=1) #one out features since its regression
+
+
+    def forward(self, x):
+      # define the forward method based on your defined network architecture
+      out = self.layer1(x)
+      out = self.layer2(out)
+      out = self.layer3(out)
+      out = self.layer4(out)
+      
+      #must resize before passing into linear layer
+    #   out = out.reshape(batch_size, self.input_fts) #TODO: implement batch training on desktop later
+      out = self.fc(out)
+      return out
--- train/train.py
+++ train/train.py
@@ -9,12 +9,15 @@ import wandb
 sys.path.append("../data")
 from data import Dataset
 sys.path.append("../model")
-from model import *
+from model import SimpleCrowdModel
 
 class Trainer():
     def __init__(self, args) -> None:
         self.args = args
         self.data = Dataset(args)
+        self.epoch_idx = self.args.epochs
+        if self.args.log_wandb == "True":
+            wandb.init(project=self.args.wandb_project, entity=self.args.wandb_entity)
 
     def build_dataset(self):
         if not self.args.load_dataset:
@@ -25,12 +28,74 @@ class Trainer():
             self.data = self.data.load()
 
     def build_model(self):
+        #load model if true
+        #else build model depending on args
+        #self, in_channels, out_channels, kernel_size, stride, padding)
+        if self.args.load_model == "True":
+            self.load_model()
+        else:
+            if self.args.model == "SimpleCrowdModel":
+                self.model = SimpleCrowdModel(in_channels = self.args.in_channels, out_channels = self.args.out_channels, kernel_size = self.args.kernel_size, stride = self.args.stride, padding = self.args.padding)
+            else:
+                raise ValueError("Model name not recognized")
+        self.model = self.model.to(args.device)
+
+    def train(self):
+        #TODO wandb logging
+        pass
+
+    def metrics(self, y_true, y_pred, x_data):
+        #TODO
+        #R2
+        #mae
+        pass
+
+    '''run model prediction on the input data'''
+    def inference(self, x_data, y_data):
         #TODO
         pass
 
-    def train(self):
+    '''runs inference on training and testing sets and collects scores for data logging purposes''' #only log to wanb during eval since thats only when u get a validation loss
+    def evaluate(self):
         #TODO
         pass
+
+    def save_model(self, is_best=False):
+        if is_best:
+            saved_path = Path(self.args.model_save_directory + self.args.model_save_file).resolve()
+        else:
+            saved_path = Path(self.args.model_save_directory + self.args.model_save_file).resolve()
+        os.makedirs(os.path.dirname(saved_path), exist_ok=True)
+        torch.save({
+            'epoch': self.epoch_idx,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            #'loss': self.metrics.best_val_loss,
+        }, str(saved_path))
+        with open(os.path.dirname(saved_path) + "/model_parameters.txt", "w+") as f:
+            f.write(str(self.args.__dict__))
+            f.write('\n')
+            f.write(str(' '.join(sys.argv)))
+        print("Model saved.")
+
+
+    '''Function to load the model, optimizer, scheduler.'''
+    def load_model(self):  
+        saved_path = Path(self.args.model_load_path).resolve()
+        if saved_path.exists():
+            self.build_model()
+            torch.cuda.empty_cache()
+            checkpoint = torch.load(str(saved_path), map_location="cpu")
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            self.epoch_idx = checkpoint['epoch']
+            #self.metrics.best_val_loss = checkpoint['loss']
+            self.model.to(self.args.device)
+            self.model.eval()
+        else:
+            raise FileNotFoundError("model load path does not exist.")
+        print("Model loaded from file.")
+
 
 if __name__ == "__main__":
         ap = ArgumentParser(description='The parameters for training.')
@@ -40,6 +105,20 @@ if __name__ == "__main__":
         ap.add_argument('--img_width', type=int, default=60, help="Resized width")
         ap.add_argument('--img_height', type=int, default=60, help="Resized height")
         ap.add_argument('--data_count', type=int, default=10, help="Number of images to extract")
+
+
+        ap.add_argument('--model', type=str, default = "SimpleCrowdModel")
+
+        ap.add_argument('--in_channels', type=int, default = 3) #rgb
+        ap.add_argument('--out_channels', type=int, default = 20) #play with this
+        ap.add_argument('--kernel_size', type=int, default = 3) #play with this
+        ap.add_argument('--stride', type=int, default = 1)
+        ap.add_argument('--padding', type=int, default = 0) 
+
+        ap.add_argument('--model_save_directory', type=str, default = "saved_models/")
+        ap.add_argument('--model_save_file', type=str, default = "best_model.pt")
+        ap.add_argument('--model_load_path', type=str, default = "saved_models/best_model.pt")
+        ap.add_argument('--load_model', type=bool, default = False)
 
 
         ap.add_argument('--epochs', type=int, default = 50)
@@ -52,12 +131,6 @@ if __name__ == "__main__":
         ap.add_argument('--log_wandb', type=str, default = "True")
         ap.add_argument('--wandb_project', type=str, default = "") #TODO
         ap.add_argument('--wandb_entity', type=str, default = "") #TODO
-
-        ap.add_argument('--model', type=str, default = "CrowdModel")
-        ap.add_argument('--model_save_directory', type=str, default = "saved_models/")
-        ap.add_argument('--model_save_file', type=str, default = "best_model.pt")
-        ap.add_argument('--model_load_path', type=str, default = "saved_models/best_model.pt")
-        ap.add_argument('--load_model', type=bool, default = False)
         
         #include more args here if we decide to do ensemble training?
 
@@ -67,7 +140,4 @@ if __name__ == "__main__":
         trainer.build_dataset()
         trainer.build_model()
         trainer.train()
-
-
-if __name__ == "__main__":
-    pass
+        trainer.save_model()
